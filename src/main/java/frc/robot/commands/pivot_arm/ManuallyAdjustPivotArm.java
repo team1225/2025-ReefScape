121992f3a70280e1c36b34458aa404e2c8a78153
Jason Heilemann <jheilemann@team1225.com>
2025-04-23 18:07:28 -0400

    4/21/2025

--- src/main/java/frc/robot/commands/pivot_arm/ManuallyAdjustPivotArm.java
+++ src/main/java/frc/robot/commands/pivot_arm/ManuallyAdjustPivotArm.java
@@ -4,16 +4,20 @@
 
 package frc.robot.commands.pivot_arm;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.PivotArm;
 
 public class ManuallyAdjustPivotArm extends Command {
   /** Creates a new JogArm. */
   private PivotArm m_arm;
   private CommandXboxController m_controller;
+  private SlewRateLimiter m_magLimiter = new SlewRateLimiter(.5);
+
 
   public ManuallyAdjustPivotArm(PivotArm arm, CommandXboxController controller) {
     m_arm = arm;
@@ -30,7 +34,7 @@ public class ManuallyAdjustPivotArm extends Command {
   @Override
   public void execute() {
     double yval = m_controller.getLeftY(); // Left stick Y axis
-    m_arm.driveManually(-yval);
+    m_arm.driveManuallyVoltage(m_magLimiter.calculate(-yval));
   }
 
   // Called once the command ends or is interrupted.
